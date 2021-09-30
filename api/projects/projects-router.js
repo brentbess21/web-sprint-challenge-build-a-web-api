420cb6e73eac5d520daf480b1337fd4f343207ac
brentbess21 <brentbess21@gmail.com>
2021-09-30 17:39:36 -0600

    Created GET /api/projects/:id endpoint. Passing tests 1-4

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -13,4 +13,20 @@ router.get('/', async (req, res, next)=> {
     }
 })
 
+router.get('/:id', async (req, res, next)=> {
+    try {
+        const { id } = req.params
+        const correctProject = await Project.get(id)
+        if(!correctProject){
+            res.status(404).json({
+                message: "project not found"
+            })
+        } else {
+            res.status(200).json(correctProject)
+        }
+    } catch (err) {
+        next(err)
+    }
+})
+
 module.exports = router;

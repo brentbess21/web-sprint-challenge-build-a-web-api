ff3f356fe0af3e3028df7e9cbc350bae2ee91c54
brentbess21 <brentbess21@gmail.com>
2021-09-30 17:33:16 -0600

    Moved GET endpoint for /api/projects to projects-router

--- api/server.js
+++ api/server.js
@@ -1,20 +1,16 @@
 const express = require('express');
 const server = express();
 
+const projectsRouter = require('./projects/projects-router');
 
-const Project = require('./projects/projects-model')
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
 
-server.get('/api/projects', async (req, res, next)=> {
-    try{
-        const projects = await Project.get()
-        res.status(200).json(projects)
-    } catch (err) {
-        next(err)
-    }
-})
+server.use(express.json());
+
+server.use('/api/projects', projectsRouter);
+
 
 module.exports = server;

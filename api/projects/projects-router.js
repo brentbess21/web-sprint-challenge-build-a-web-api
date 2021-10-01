c2d50a6a30d4d3d6fe15d48d0787fba9b6623926
brentbess21 <brentbess21@gmail.com>
2021-10-01 12:59:00 -0600

    Created final GET endpoint in projects-router. Tests 1-16 are passing

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -57,6 +57,14 @@ router.delete('/:id', checkProjectId, async (req, res, next)=> {
     }
 })
 
-
+router.get('/:id/actions', checkProjectId, async (req, res, next)=> {
+    try {
+        const { id } = req.params;
+        const projectActions = await Project.getProjectActions(id);
+        res.status(200).json(projectActions);
+    } catch (err) {
+        next(err)
+    }
+})
 
 module.exports = router;

f0540ae3e976cd0e544f69baa97a90cb58ed126b
brentbess21 <brentbess21@gmail.com>
2021-10-01 09:56:20 -0600

    Added a logger middleware and applied it to projects router in server.js

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,14 @@
 // add middlewares here related to projects
+
+
+
+function logger (req, res, next) {
+    const currentTime = Date().toLocaleString()
+    console.log(`There was a ${req.method} request to ${req.originalUrl} at ${currentTime}`)
+    next() 
+  }
+
+
+  module.exports = {
+      logger,
+  }
--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const server = express();
 
 const projectsRouter = require('./projects/projects-router');
+const { logger } = require('./projects/projects-middleware');
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
@@ -10,7 +11,7 @@ const projectsRouter = require('./projects/projects-router');
 
 server.use(express.json());
 
-server.use('/api/projects', projectsRouter);
+server.use('/api/projects', logger, projectsRouter);
 
 
 module.exports = server;

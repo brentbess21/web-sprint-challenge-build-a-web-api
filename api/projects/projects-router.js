3525b8f948a892055543e9074b788ac991c99e73
brentbess21 <brentbess21@gmail.com>
2021-10-01 10:46:17 -0600

    Created POST endpoint. Test 7 isn't passing though

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -5,6 +5,7 @@ const router = express.Router()
 const Project = require('./projects-model')
 const { checkProjectId } = require('./projects-middleware')
 
+//GET endpoints
 router.get('/', async (req, res, next)=> {
     try{
         const projects = await Project.get()
@@ -16,6 +17,26 @@ router.get('/', async (req, res, next)=> {
 
 router.get('/:id', checkProjectId, async (req, res)=> {
     res.status(200).json(req.project)
+})
+
+//POST endpoint
+
+router.post('/', async (req, res, next)=> {
+    try {
+        const { name, description } = req.body;
+        const newProject = await Project.insert(req.body)
+
+        if(name || description) {
+            res.status(201).json(newProject)
+            
+        } else {
+            res.status(400).json({
+                message: "Please provide a name and description"
+            })
+        }
+    } catch (err){
+        next(err)
+    }
 })
 
 module.exports = router;
